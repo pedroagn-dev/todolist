61ce45e43e80028ba222ea68b60a1de0695afee2
Pedro Augusto Galhardo Nogueira <pedro@pop-os.localdomain>
2023-03-18 19:27:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        if (!inputValue.trim()) {
+            return;
+        }
+        setTasks([...tasks, { id: Date.now(), text: inputValue.trim(), completed: false }]);
+        setInputValue('');
+    };
+
+    const handleTaskDelete = (taskId) => {
+        const filteredTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(filteredTasks);
+    };
+
+    const handleTaskToggle = (taskId) => {
+        const updatedTasks = tasks.map((task) => {
+            if (task.id === taskId) {
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    };
+
+    const handleAllTasksComplete = () => {
+        const completedTasks = tasks.map(task => {
+            return {...task, completed: true};
+        });
+        setTasks(completedTasks);
+    };
+
+    return (
+        <div className="container">
+            <h2>Todo List</h2>
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    placeholder="Escreva uma nova tarefa!"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Adicionar</button>
+            </form>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <label>
+                            <input type="checkbox" checked={task.completed} onChange={() => handleTaskToggle(task.id)} />
+                            {task.text}
+                        </label>
+                        <button onClick={() => handleTaskDelete(task.id)}>
+                            <FontAwesomeIcon icon={faTrash} />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <div className="button-container">
+                <button
+                    className="delete-completed"
+                    onClick={() => setTasks(tasks.filter(task => !task.completed))}
+                >
+                    Excluir tarefas já feitas
+                </button>
+                <button
+                    className="complete-all"
+                    onClick={handleAllTasksComplete}
+                >
+                    Concluir todas as tarefas
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
